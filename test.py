78fb789aeb29daa0ca395c50de651d02cee3d383
ying871797 <ying871797@126.com>
2025-02-07 16:56:52 +0800

    refactor(op.py): 使用环境变量配置数据库连接

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
 # -*- coding: UTF-8 -*-
-import pandas as pd
+from os.path import dirname, abspath
 
-data = {
-    'date': ['2024-05-26'],
-    'ip': ['0.0.0.0'],
-    'address': ['未知'],
-    'chatroom': ['class6']
-}
-df = pd.DataFrame(data)
-df.to_csv('test.csv', mode='a', index=False, header=False, encoding='utf-8')
+path = dirname(dirname(abspath(__file__))) + '\ourforum\public\graphs.html'
+print(path)
--- test0.py
+++ test0.py
@@ -9,19 +9,27 @@
 import json
 from datetime import datetime
 
-import pymysql
+# import pymysql
+#
+# from op import Mysql
+#
+# db_ = Mysql()
+# db = pymysql.connect(host='43.143.201.36',
+#                      user='root',
+#                      password='xiang090526',
+#                      database='ourforum')
+# cursor = db.cursor()
+# # cursor.execute('select * from data')
+# # data = cursor.fetchall()
+# data = db_.get_all('ipblacklist')
+# print(data)
+# db.commit()
+# db.close()
+import re
 
-from op import Mysql
-
-db_ = Mysql()
-db = pymysql.connect(host='43.143.201.36',
-                     user='root',
-                     password='xiang090526',
-                     database='ourforum')
-cursor = db.cursor()
-# cursor.execute('select * from data')
-# data = cursor.fetchall()
-data = db_.get_all('ipblacklist')
-print(data)
-db.commit()
-db.close()
+with open('./logs/20241003.log', 'r', encoding='utf-8') as f:
+    txt = f.read()
+    res = re.sub('\s+', ',', txt)
+    print(res)
+    with open('./logs/20241003.csv', 'w', encoding='utf-8') as f:
+        f.write(res)
